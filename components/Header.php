946421d679311ca903559f210983727c22dfeeba
Dima Shamoev <dimashamoev5@gmail.com>
2024-12-24 22:00:54 +0400

    Created useful folders, created header

---
+++ components/Header.php
@@ -0,0 +1,50 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="css/components.css">
+</head>
+<header class="header">
+    <div class="header-top">
+        <div class="container">
+            <div class="header-top-wrapper">
+                <div class="header-top-left-side">
+                    <ul>
+                        <li>Hi <a class="underline-link" href="#">Sign In</a> Or <a class="underline-link" href="#">Sign Up</a></li>
+                        <li><a href="#">Daily Deals</a></li>
+                        <li><a href="#">Help & Contact</a></li>
+                    </ul>
+                </div>
+                <div class="header-top-right-side">
+                    <ul>
+                        <li><a href="#">Sell</a></li>
+                        <li><a href="#">Watchlist <i class='bx bx-chevron-down'></i></a></li>
+                        <li><a href="#">My eBay <i class='bx bx-chevron-down'></i></a></li>
+                        <li><a href="#"><i class='bx bx-bell'></i></a></li>
+                        <li><a href="#"><i class='bx bx-cart'></i></a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="header-bottom">
+        <div class="container">
+            <div class="header-bottom-wrapper">
+                <div class="header-bottom-logo">
+                    <a href="#">
+                        <img src="images/ebay-logo.svg" height="60" alt="">
+                    </a>
+                </div>
+                <div class="header-bottom-search-block">
+                    <div class="header-bottom-search-input">
+                        <i class='bx bx-search-alt-2'></i> <input type="text">
+                    </div>
+                    <div class="header-bottom-search-btn">
+                        search
+                    </div>
+                </div>
+                <div class="hidden-block"></div>
+            </div>
+        </div>
+    </div>
+</header>
--- index.php
+++ index.php
@@ -3,9 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/index.css">
     <title>MyEbay</title>
 </head>
 <body>
-    
+    <?php include "./components/Header.php"?>
 </body>
 </html>
