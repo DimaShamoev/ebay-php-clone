ce09680f59677da494b8ebe8f01238a00e289b36
Dima Shamoev <dimashamoev5@gmail.com>
2024-12-25 16:34:06 +0400

    added element on main-first page

--- index.php
+++ index.php
@@ -4,21 +4,52 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./css/index.css">
+    <link rel="icon" type="png" href="./images/ebay.png">
     <title>MyEbay</title>
 </head>
 <body>
-    <?php
-        include "./components/Header.php"
-    ?>
+    
 
-    <br>
-    <br>
-    <br>
+    <div class="body-wrapper">
 
+        <?php
+            include "./components/Header.php"
+        ?>
 
-    <?php
-        include "./components/Footer.php"
-    ?>
+        <main>
+
+            <div class="container">
+
+                <div class="main-wrapper">
+
+                    <div class="main-page-navigation">
+                        <ul>
+                            <li><a href="#">Explore (New!)</a></li>
+                            <li><a href="#">Saved</a></li>
+                            <li><a href="#">Electronics</a></li>
+                            <li><a href="#">Motors</a></li>
+                            <li><a href="#">Fashion</a></li>
+                            <li><a href="#">Collectibles and Art</a></li>
+                            <li><a href="#">Sports</a></li>
+                            <li><a href="#">Health & Beauty</a></li>
+                            <li><a href="#">Industrial equipment</a></li>
+                            <li><a href="#">Home & Garden</a></li>
+                            <li><a href="#">Deals</a></li>
+                            <li><a href="#">Sell</a></li>
+                        </ul>
+                    </div>
+
+                </div>
+
+            </div>
+
+        </main>
+
+        <?php
+            include "./components/Footer.php"
+        ?>
+
+    </div>
 
 
 </body>
