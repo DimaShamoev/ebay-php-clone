3408c431069150d192bbd23015739072a12491bc
Dima Shamoev <dimashamoev5@gmail.com>
2024-12-26 13:17:20 +0400

    finished main page UI

--- index.php
+++ index.php
@@ -39,14 +39,108 @@
                         </ul>
                     </div>
 
-                    <div class="main-page-banner">
-                        <div class="main-page-banner-title">
+                    <div class="main-page-banner banner-block">
+                        <div class="main-page-banner-title banner-title">
                             You don't run with the crowd
                         </div>
-                        <div class="main-page-banner-text">
+                        <div class="main-page-banner-text banner-text">
                             Get fit your own way with health and wellness gear on eBay.
                         </div>
-                        <div class="main-page-banner-text">
+                        <div class="main-page-banner-text banner-text">
+                            <a href="#">Shop For Your Style</a>
+                        </div>
+                    </div>
+
+                    <div class="popular-categories-block">
+                        <div class="popular-categories-title">
+                            <h4>Explore Popular Categories</h4>
+                        </div>
+                        <div class="popular-categories-type">
+                            <div class="popular-category popular-category-1">
+                                <div class="popular-category-img">
+                                    <img src="images/popular-categories-1.jfif" alt="Luxury">
+                                </div>
+                                <div class="popular-category-name">
+                                    Luxury
+                                </div>
+                            </div>
+
+                            <div class="popular-category popular-category-2">
+                                <div class="popular-category-img">
+                                    <img src="images/popular-categories-2.jpg" alt="Sneakers">
+                                </div>
+                                <div class="popular-category-name">
+                                    Sneakers
+                                </div>
+                            </div>
+
+                            <div class="popular-category popular-category-3">
+                                <div class="popular-category-img">
+                                    <img src="images/popular-categories-3.jpg" alt="P&A">
+                                </div>
+                                <div class="popular-category-name">
+                                    P&A
+                                </div>
+                            </div>
+
+                            <div class="popular-category popular-category-4">
+                                <div class="popular-category-img">
+                                    <img src="images/popular-categories-4.jpg" alt="Refurbished">
+                                </div>
+                                <div class="popular-category-name">
+                                    Refurbished
+                                </div>
+                            </div>
+
+                            <div class="popular-category popular-category-5">
+                                <div class="popular-category-img">
+                                    <img src="images/popular-categories-5.jpg" alt="Trading cards">
+                                </div>
+                                <div class="popular-category-name">
+                                    Trading cards
+                                </div>
+                            </div>
+
+                            <div class="popular-category popular-category-6">
+                                <div class="popular-category-img">
+                                    <img src="images/popular-categories-6.jpg" alt="Pre-loved Luxury">
+                                </div>
+                                <div class="popular-category-name">
+                                    Pre-loved Luxury
+                                </div>
+                            </div>
+
+                            <div class="popular-category popular-category-7">
+                                <div class="popular-category-img">
+                                    <img src="images/popular-categories-7.jpg" alt="Toys">
+                                </div>
+                                <div class="popular-category-name">
+                                    Toys
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="get-fit-banner banner-block">
+                        <div class="main-page-banner-title banner-title">
+                            You don't run with the crowd
+                        </div>
+                        <div class="main-page-banner-text banner-text">
+                            Get fit your own way with health and wellness gear on eBay.
+                        </div>
+                        <div class="main-page-banner-text banner-text">
+                            <a href="#">Shop For Your Style</a>
+                        </div>
+                    </div>
+
+                    <div class="discover-banner banner-block">
+                        <div class="main-page-banner-title banner-title">
+                            You don't run with the crowd
+                        </div>
+                        <div class="main-page-banner-text banner-text">
+                            Get fit your own way with health and wellness gear on eBay.
+                        </div>
+                        <div class="main-page-banner-text banner-text">
                             <a href="#">Shop For Your Style</a>
                         </div>
                     </div>
