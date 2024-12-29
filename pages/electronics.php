a4815a2b54480ce521fdafffb5b4cab5b42b7fbf
Dima Shamoev <dimashamoev5@gmail.com>
2024-12-29 13:32:12 +0400

    created aside on electronics page

--- pages/electronics.php
+++ pages/electronics.php
@@ -16,6 +16,88 @@
             include "../components/Header.php"
         ?>
 
+        <main class="main">
+            <div class="container">
+
+                <div class="main-wrapper">
+
+                    <div class="main-top-banner">
+
+                        <div class="main-top-banner-title">Welcome to the world of tech</div>
+                        <div class="main-top-banner-text">Browse all the devices you need, from smartphones to PCs.</div>
+
+                    </div>
+                    <div class="main-shop-content">
+
+                        <div class="shop-list-side">
+                            <ul>
+                                <li><p>Shop by Category</p></li>
+                                <li><a href="#">Cameras & Photo</a></li>
+                                <li><a href="#">Vehicle Electronics & GPS</a></li>
+                                <li><a href="#">Home Surveillance Systems</a></li>
+                                <li><a href="#">Surveillance & Smart Home Electronics</a></li>
+                                <li><a href="#">Major Appliances, Parts & Accessories</a></li>
+                                <li><a href="#">Video Games & Consoles</a></li>
+                                <li><a href="#">Virtual Reality Headsets, Parts & Accessories</a></li>
+                                <li><a href="#">Cell Phones, Smart Watches & Accessories</a></li>
+                                <li><a href="#">Computers, Tablets & Network Hardware</a></li>
+                                <li><a href="#">TV, Video & Home Audio Electronics</a></li>
+                                <li><a href="#">Portable Audio & Headphones</a></li>
+                            </ul>
+                            
+                            <div class="list-side-line"></div>
+
+                            <ul>
+                                <li><p>Your favorite brands</p></li>
+                                <li><a href="#">Cameras & Photo</a></li>
+                                <li><a href="#">Vehicle Electronics & GPS</a></li>
+                                <li><a href="#">Home Surveillance Systems</a></li>
+                                <li><a href="#">Surveillance & Smart Home Electronics</a></li>
+                                <li><a href="#">Major Appliances, Parts & Accessories</a></li>
+                                <li><a href="#">Video Games & Consoles</a></li>
+                                <li><a href="#">Virtual Reality Headsets, Parts & Accessories</a></li>
+                                <li><a href="#">Cell Phones, Smart Watches & Accessories</a></li>
+                                <li><a href="#">Computers, Tablets & Network Hardware</a></li>
+                                <li><a href="#">TV, Video & Home Audio Electronics</a></li>
+                                <li><a href="#">Portable Audio & Headphones</a></li>
+                            </ul>
+
+                            <div class="list-side-line"></div>
+
+                            <ul>
+                                <li><p>Shop by Interest</p></li>
+                                <li><a href="#">Cameras & Photo</a></li>
+                                <li><a href="#">Vehicle Electronics & GPS</a></li>
+                                <li><a href="#">Home Surveillance Systems</a></li>
+                                <li><a href="#">Surveillance & Smart Home Electronics</a></li>
+                                <li><a href="#">Major Appliances, Parts & Accessories</a></li>
+                                <li><a href="#">Video Games & Consoles</a></li>
+                                <li><a href="#">Virtual Reality Headsets, Parts & Accessories</a></li>
+                                <li><a href="#">Cell Phones, Smart Watches & Accessories</a></li>
+                                <li><a href="#">Computers, Tablets & Network Hardware</a></li>
+                                <li><a href="#">TV, Video & Home Audio Electronics</a></li>
+                                <li><a href="#">Portable Audio & Headphones</a></li>
+                            </ul>
+
+                        </div>
+                        <div class="shop-content-side">
+                            <div class="shop-content-side-top-banner">
+                                <a href="#">
+                                    <img width="100%" src="/ebay-php-clone/images/electronics-content-banner1.webp" alt="">
+                                </a>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+
+            </div>
+        </main>
+
+        <?php 
+            include "../components/Footer.php"
+        ?>
+
     </div>
 </body>
 </html>
