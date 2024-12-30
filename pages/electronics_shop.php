de9c80b6066d5eef86632548b325ee2a353ad1f3
Dima Shamoev <dimashamoev5@gmail.com>
2024-12-30 18:08:41 +0400

    cleaned and refreshed

--- pages/electronics_shop.php
+++ pages/electronics_shop.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -9,58 +10,61 @@
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     <title>Electronics Shop</title>
 </head>
+
 <body>
-    
-    <div class="container">
 
     <div class="body-wrapper">
 
         <?php include "../components/Header.php" ?>
 
         <main class="main">
-            
-            <div class="sort-by-controllers-block">
-                
-            </div>
-
-            <div class="shop-body">
-
-                <div class="shop-filter">
-                    <ul>
-                        <li><b>Type</b></li>
-                        <li><a href="#">Mobiles</a></li>
-                        <li><a href="#">Tablets/Ipads</a></li>
-                        <li><a href="#">Laptops</a></li>
-                        <li><a href="#">PCs</a></li>
-                        <li><a href="#">TVs</a></li>
-                        <li><a href="#">Consoles</a></li>
-                        <li><a href="#">Headsets</a></li>
-                        <li><a href="#">Smartwatches</a></li>
-                    </ul>
-
-                    <ul>
-                        <li><b>Price</b></li>
-                        <li><a href="#">Mobiles</a></li>
-                        <li><a href="#">Tablets/Ipads</a></li>
-                        <li><a href="#">Laptops</a></li>
-                        <li><a href="#">PCs</a></li>
-                        <li><a href="#">TVs</a></li>
-                        <li><a href="#">Consoles</a></li>
-                        <li><a href="#">Headsets</a></li>
-                        <li><a href="#">Smartwatches</a></li>
-                    </ul>
-
-                    
-                </div>
+            <div class="container">
 
 
-                <div class="shop-products">
-
-
+                <div class="sort-by-controllers-block">
 
                 </div>
 
+                <div class="shop-body">
+
+                    <div class="shop-filter">
+                        <ul>
+                            <li><b>Type</b></li>
+                            <li><a href="#">Mobiles</a></li>
+                            <li><a href="#">Tablets/Ipads</a></li>
+                            <li><a href="#">Laptops</a></li>
+                            <li><a href="#">PCs</a></li>
+                            <li><a href="#">TVs</a></li>
+                            <li><a href="#">Consoles</a></li>
+                            <li><a href="#">Headsets</a></li>
+                            <li><a href="#">Smartwatches</a></li>
+                        </ul>
+
+                        <ul>
+                            <li><b>Price</b></li>
+                            <li><a href="#">Mobiles</a></li>
+                            <li><a href="#">Tablets/Ipads</a></li>
+                            <li><a href="#">Laptops</a></li>
+                            <li><a href="#">PCs</a></li>
+                            <li><a href="#">TVs</a></li>
+                            <li><a href="#">Consoles</a></li>
+                            <li><a href="#">Headsets</a></li>
+                            <li><a href="#">Smartwatches</a></li>
+                        </ul>
+
+
+                    </div>
+
+
+                    <div class="shop-products">
+
+
+
+                    </div>
+
+                </div>
             </div>
+
 
         </main>
 
@@ -68,7 +72,7 @@
 
     </div>
 
-    </div>
 
 </body>
+
 </html>
