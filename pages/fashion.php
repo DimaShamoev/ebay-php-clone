8a212193c6f883ad301b6d8e6571cd6060df5a4e
Dima Shamoev <dimashamoev5@gmail.com>
2024-12-31 13:35:18 +0400

    finished fashion page

---
+++ pages/fashion.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="png" href="../images/ebay.png">
+    <link rel="stylesheet" href="../css/fashion.css">
+    <link rel="stylesheet" href="../css/components.css">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <title>Document</title>
+</head>
+<body>
+    <div class="body-wrapper">
+
+        <?php include "../components/Header.php" ?>
+        <main class="main">
+            <div class="container">
+                <div class="main-wrapper">
+                    <div class="fashion-title">
+                        Fashion
+                    </div>
+
+                    <div class="fashion-offers">
+                        <div class="fashion-offer">
+                            <a href="#">
+                                <img src="/ebay-php-clone/images/fashion-1.png" alt="">
+                            </a>
+                        </div>
+                        <div class="fashion-offer">
+                            <a href="#">
+                                <img src="/ebay-php-clone/images/fashion-2.png" alt="">
+                            </a>
+                        </div>
+                        <div class="fashion-offer">
+                            <a href="#">
+                                <img src="/ebay-php-clone/images/fashion-3.png" alt="">
+                            </a>
+                        </div>
+                        <div class="fashion-offer">
+                            <a href="#">
+                                <img src="/ebay-php-clone/images/fashion-4.png" alt="">
+                            </a>
+                        </div>
+                        <div class="fashion-offer">
+                            <a href="#">
+                                <img src="/ebay-php-clone/images/fashion-5.png" alt="">
+                            </a>
+                        </div>
+                        <div class="fashion-offer">
+                            <a href="#">
+                                <img src="/ebay-php-clone/images/fashion-6.png" alt="">
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </main>
+        <?php include "../components/Footer.php" ?>
+
+    </div>
+</body>
+</html>
